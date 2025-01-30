f7f7216a7a555d9d306924cef26881a1b9a2582b
Fabio Camargo <fabiorcamargo@gmail.com>
2025-01-30 14:09:24 -0300

    up

---
+++ app/zap/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useEffect } from 'react';
+
+const Page = () => {
+  useEffect(() => {
+    window.location.href = 'https://wa.me/5544984615290'; // Altere para o link desejado
+  }, []);
+};
+
+export default Page;
