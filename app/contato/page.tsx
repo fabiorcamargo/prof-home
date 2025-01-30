1a11898f297596c11544f7bb07fc0ce9f2bc0244
Fabio Camargo <fabiorcamargo@gmail.com>
2025-01-30 14:00:51 -0300

    up

---
+++ app/contato/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useEffect } from 'react';
+
+const Page = () => {
+  useEffect(() => {
+    window.location.href = 'https://nwdsk.co/c17SA'; // Altere para o link desejado
+  }, []);
+};
+
+export default Page;
