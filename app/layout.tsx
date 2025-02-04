18f20bc0eb618ae0e079c7a902eabbb23e1b00c3
Fabio Camargo <fabiorcamargo@gmail.com>
2025-02-04 01:00:20 -0300

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,16 @@ export const metadata = {
     description: 'Cursos para o seu futuro',
     images: [
       {
-        url: 'https://home.profissionalizaead.com.br/Logo.png',
+        url: 'https://home.profissionalizaead.com.br/Logo1200x630.png.png',
         width: 1200,
         height: 630,
         alt: 'Logo Profissionaliza EAD',
+      },
+      {
+        url: 'https://home.profissionalizaead.com.br/Thumbnail.png',
+        width: 600,
+        height: 315,
+        alt: 'Thumbnail Profissionaliza EAD',
       },
     ],
     type: 'website',
@@ -44,7 +50,6 @@ export default function RootLayout({
     <html lang="pt-BR">
       <Head>
         {/* Favicon */}
-
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
@@ -52,10 +57,16 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="Pro EAD" />
         <link rel="manifest" href="/site.webmanifest" />
 
+        {/* Meta Tags SEO */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Profissionaliza EAD" />
+        <meta name="robots" content="index, follow" />
+        <meta name="theme-color" content="#fdc500" />
+
         {/* Canonical */}
         <link rel="canonical" href="https://profissionalizaead.com.br" />
 
-        
+        {/* JSON-LD */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -68,22 +79,23 @@ export default function RootLayout({
               "sameAs": [
                 "https://www.facebook.com/ProfissionalizaEAD",
                 "https://www.instagram.com/ProfissionalizaEAD",
-                "https://www.linkedin.com/company/profissionalizaead"
-              ]
+                "https://www.linkedin.com/company/profissionalizaead",
+              ],
             }),
           }}
         />
       </Head>
       <body>
         <header>
-          {/* Adicione elementos globais do cabeçalho aqui, se necessário */}
+          {/* Elementos globais do cabeçalho podem ser adicionados aqui */}
         </header>
         <main>{children}</main>
         <footer>
-          {/* Rodapé do site */}
+          {/* Adicione elementos do rodapé aqui */}
         </footer>
-        
-        
+
+        {/* Scripts externos (lazy carregamento) */}
+        <Script src="https://example.com/some-script.js" strategy="lazyOnload" />
       </body>
     </html>
   );
